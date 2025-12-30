28894bf7ffadc904dbf44089d00eea2bb6203503
Irma Daniela Lopez Toscano <danielasheart@gmail.com>
2025-12-30 14:35:15 -0600

    Not show continous ping fix 3

--- C_Mosquitto/client.c
+++ C_Mosquitto/client.c
@@ -2,11 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <pthread.h>
 #include <mosquitto.h>
 
 #define BROKER_HOST "192.168.100.10"
 #define BROKER_PORT 1883
 #define KEEPALIVE   60
+
+static struct mosquitto *mosq;
+static int running = 1;
+
+/* ---------- Callbacks ---------- */
 
 static void on_connect(struct mosquitto *mosq, void *obj, int rc)
 {
@@ -30,13 +36,19 @@ static void on_disconnect(struct mosquitto *mosq, void *obj, int rc)
     printf("[+] Disconnected from broker (rc=%d)\n", rc);
 }
 
+/* ---------- MQTT loop thread ---------- */
+
+void *mqtt_loop_thread(void *arg)
+{
+    mosquitto_loop_forever(mosq, -1, 1);
+    return NULL;
+}
+
+/* ---------- MAIN ---------- */
+
 int main(void)
 {
-    struct mosquitto *mosq;
-    int rc;
-    int running = 1;
-
-    /* ---------- client_id ÚNICO ---------- */
+    pthread_t mqtt_thread;
     char client_id[64];
     snprintf(client_id, sizeof(client_id), "client_%d", getpid());
 
@@ -55,12 +67,15 @@ int main(void)
     mosquitto_message_callback_set(mosq, on_message);
     mosquitto_disconnect_callback_set(mosq, on_disconnect);
 
-    rc = mosquitto_connect(mosq, BROKER_HOST, BROKER_PORT, KEEPALIVE);
-    if(rc != MOSQ_ERR_SUCCESS) {
-        fprintf(stderr, "[-] Unable to connect (%s)\n", mosquitto_strerror(rc));
+    if(mosquitto_connect(mosq, BROKER_HOST, BROKER_PORT, KEEPALIVE) != MOSQ_ERR_SUCCESS) {
+        fprintf(stderr, "[-] Unable to connect\n");
         return 1;
     }
 
+    /* ---------- Start MQTT loop thread ---------- */
+    pthread_create(&mqtt_thread, NULL, mqtt_loop_thread, NULL);
+
+    /* ---------- Menu ---------- */
     while(running) {
         int choice;
         char topic[64];
@@ -99,17 +114,11 @@ int main(void)
             topic[strcspn(topic, "\n")] = 0;
 
             mosquitto_subscribe(mosq, NULL, topic, 1);
-            printf("[+] Subscribed to topic: %s\n", topic);
+            printf("[+] Subscribed to %s\n", topic);
         }
         else if(choice == 3) {
             running = 0;
         }
-        else {
-            printf("[-] Invalid option\n");
-        }
-
-        /* ---------- loop MQTT ESTABLE ---------- */
-        mosquitto_loop(mosq, 100, 1);
     }
 
     mosquitto_disconnect(mosq);
