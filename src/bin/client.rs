9b28fde88e88fac5591824f5a02ae304e5de1824
Irma Daniela Lopez Toscano <danielasheart@gmail.com>
2026-02-12 16:37:49 -0600

    Timers on publish pckg

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -166,11 +166,44 @@ fn start_client()
             }
 
             if mode == "pub" {
-                send_publish_packet(
-                    stream.try_clone().unwrap(),
-                    "test",
-                    "hola"
-                );
+                let payload_size: usize = args.get(2)
+                        .expect("Missing payload size")
+                        .parse()
+                        .expect("Invalid payload size");
+
+                    let execution_time: u64 = args.get(3)
+                        .expect("Missing execution time")
+                        .parse()
+                        .expect("Invalid execution time");
+
+                    let publish_frequency: u64 = args.get(4)
+                        .expect("Missing publish frequency")
+                        .parse()
+                        .expect("Invalid publish frequency");
+
+                    let publish_interval = Duration::from_secs(publish_frequency);
+
+                    // Genera automáticamente el payload con el tamaño indicado
+                    let payload = "A".repeat(payload_size);
+
+                    let program_start = Instant::now();
+
+                    while program_start.elapsed() < Duration::from_secs(execution_time) {
+
+                        let publish_start = Instant::now();
+
+                        send_publish_packet(
+                            stream.try_clone().unwrap(),
+                            "test",
+                            &payload
+                        );
+
+                        let elapsed = publish_start.elapsed();
+
+                        if elapsed < publish_interval {
+                            thread::sleep(publish_interval - elapsed);
+                        }
+                    }
             }
 
             let listener_stream = stream.try_clone().unwrap();
