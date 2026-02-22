3bd45f1aad3be6c23049f54dba856cb56e4de578
Irma Daniela Lopez Toscano <danielasheart@gmail.com>
2026-02-21 19:22:46 -0600

    Finished bash script

--- Bash_scripts/run_c_clients.sh
+++ Bash_scripts/run_c_clients.sh
@@ -12,11 +12,18 @@ CLIENT_BIN="$CLIENT_PATH/client"
 echo "Compiling..."
 gcc "$CLIENT_PATH/client.c" -o "$CLIENT_BIN" -lmosquitto
 
+# ================================
+# Guardar PIDs
+# ================================
+SUB_PIDS=()
+PUB_PIDS=()
+
 echo "Starting subscribers..."
 
 for ((i=0;i<NUM_SUB;i++))
 do
     "$CLIENT_BIN" sub $i &
+    SUB_PIDS+=($!)
 done
 
 sleep 2
@@ -26,6 +33,29 @@ echo "Starting publishers..."
 for ((i=0;i<NUM_PUB;i++))
 do
     "$CLIENT_BIN" pub $i "$PAYLOAD" "$EXEC_TIME" "$FREQ" &
+    PUB_PIDS+=($!)
+done
+
+# ================================
+# Esperar a que terminen publishers
+# ================================
+echo "Waiting publishers to finish..."
+for pid in "${PUB_PIDS[@]}"
+do
+    wait $pid
+done
+
+echo "Publishers finished."
+
+# ================================
+# Terminar subscribers
+# ================================
+echo "Stopping subscribers..."
+for pid in "${SUB_PIDS[@]}"
+do
+    kill $pid 2>/dev/null
 done
 
 wait
+
+echo "Test finished successfully."
